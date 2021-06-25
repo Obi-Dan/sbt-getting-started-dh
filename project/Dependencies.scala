af080c1c1c6b20b6468622cb276e1f25775af6e9
Daniel Hofferbert <daniel.hofferbert@daugherty.com>
2021-06-25 13:37:52 -0500

    Updated travis ci

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,6 +4,10 @@ object Dependencies {
   val scalaRequests = "com.lihaoyi" %% "requests" % "0.1.7"
   val scalaXml = "org.scala-lang.modules" %% "scala-xml" % "1.1.1"
   val scalaTest = "org.scalatest" %% "scalatest" % "3.0.5"
+  val akka = "com.typesafe.akka"
+  val akkaHttp = akka %% "akka-http" % "10.1.8"
+  val akkaStream = akka %% "akka-stream" % "2.5.19"
+  val json4s = "org.json4s" %% "json4s-native" % "3.6.5"
 
   val common: Seq[ModuleID] = Seq(
     scalaTest % Test
@@ -11,7 +15,10 @@ object Dependencies {
 
   val calculator = common
   val api = Seq(
-    scalaRequests,
-    scalaXml
+    scalaRequests
+    ,scalaXml
+    ,json4s
+    ,akkaHttp
+    ,akkaStream
   ) ++ common
 }
